fe32ea8650ef56ef0ca299c8250ee56457cd0528
aohara-cc <aohara@camcloud.com>
2021-06-27 15:02:22 -0400

    build and distribute native images

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     kotlin("jvm") version "1.4.32"
     application
     id("org.openjfx.javafxplugin") version "0.0.9"
+    id("org.beryx.runtime") version "1.12.5"
 }
 
 repositories {
@@ -30,4 +31,58 @@ javafx {
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
     kotlinOptions.jvmTarget = "11"
+}
+
+runtime {
+    addOptions("--strip-debug", "--compress", "2", "--no-header-files", "--no-man-pages")
+
+// Uncomment and adjust the code below if you want to generate images for multiple platforms.
+// (You need to also uncomment the line 'targetPlatformName = ...' in the jpackage block.)
+/*
+    targetPlatform("lin") {
+        jdkHome = jdkDownload("https://github.com/AdoptOpenJDK/openjdk15-binaries/releases/download/jdk-15.0.2%2B7/OpenJDK15U-jdk_x64_linux_hotspot_15.0.2_7.tar.gz")
+    }
+    targetPlatform("mac") {
+        jdkHome = jdkDownload("https://github.com/AdoptOpenJDK/openjdk15-binaries/releases/download/jdk-15.0.2%2B7/OpenJDK15U-jdk_x64_mac_hotspot_15.0.2_7.tar.gz") {
+            downloadDir = "$buildDir/myMac"
+            archiveName = "my-mac-jdk"
+            archiveExtension = "tar.gz"
+            pathToHome = "jdk-15.0.2+7/Contents/Home"
+            overwrite = true
+        }
+    }
+    targetPlatform("win") {
+        jdkHome = jdkDownload("https://github.com/AdoptOpenJDK/openjdk15-binaries/releases/download/jdk-15.0.2%2B7/OpenJDK15U-jdk_x64_windows_hotspot_15.0.2_7.zip")
+    }
+*/
+
+    launcher {
+        noConsole = true
+    }
+    jpackage {
+        // Uncomment and adjust the following line if your runtime task is configured to generate images for multiple platforms
+        // targetPlatformName = "mac"
+
+        val currentOs = org.gradle.internal.os.OperatingSystem.current()
+        val imgType = when {
+            currentOs.isWindows -> "ico"
+            currentOs.isMacOsX -> "icns"
+            else -> "png"
+        }
+
+        imageOptions.addAll(listOf("--icon", "src/main/resources/hellofx.$imgType"))
+
+        installerOptions.addAll(listOf(
+            "--resource-dir", "src/main/resources",
+            "--vendor", "andrewohara.io"
+        ))
+
+        val osOptions = when {
+            currentOs.isWindows -> listOf("--win-per-user-install", "--win-dir-chooser", "--win-menu", "--win-shortcut")
+            currentOs.isLinux -> listOf("--linux-package-name", "betelgeuse", "--linux-shortcut")
+            currentOs.isMacOsX -> listOf("--mac-package-name", "betelgeuse")
+            else -> listOf()
+        }
+        installerOptions.addAll(osOptions)
+    }
 }
